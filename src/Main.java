e7ba373ef60db9bdb64543775d71b9615f039241
Miika Melama <miika.melama@gmail.com>
2019-05-13 23:12:52 +0300

    Testing test2 branch

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is github test");
         System.out.println("Is this working?");
+        System.out.println("Second branch?");
     }
 }
